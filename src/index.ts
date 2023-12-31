c0151fc58e680e7d98fe23bad1491b8afcc78ee7
Walter Blacke <walter.blacke@vegabyte.studio>
2023-12-31 13:03:44 -0500

    working example

--- src/index.ts
+++ src/index.ts
@@ -20,83 +20,106 @@ const { setState } = createRoot(() => {
     },
   );
 
-  const getAllEntityIdAndState = createMemo(mapArray(
-    () => entityChunkState.ids,
-    (id, index) => () => [id, entityChunkState.entities[id]]
-  ))
+  const getAllEntityIdAndState = createMemo(
+    mapArray(
+      () => entityChunkState.ids,
+      (id, index) => () => [id, entityChunkState.entities[id]],
+    ),
+  );
 
-  const getAllEntityIdAndCounter = createMemo(mapArray(
-    () => entityChunkState.ids,
-    (id, index) => () => [id, entityChunkState.entities[id].counter]
-  ))
+  const getAllEntityIdAndCounter = createMemo(
+    mapArray(
+      () => entityChunkState.ids,
+      (id, index) => () => [id, entityChunkState.entities[id].counter],
+    ),
+  );
 
-  const getAllEntityIdAndColor = createMemo(mapArray(
-    () => entityChunkState.ids,
-    (id, index) => () => [id, entityChunkState.entities[id].color]
-  ))
-
-  createEffect(() => {
-    console.log(`getAllEntityId`, entityChunkState.ids)
-  })
-
-  createEffect(() => {
-    console.log(`getAllEntityIdAndState`, getAllEntityIdAndState().map(i => i()))
-  })
+  const getAllEntityIdAndColor = createMemo(
+    mapArray(
+      () => entityChunkState.ids,
+      (id, index) => () => [id, entityChunkState.entities[id].color],
+    ),
+  );
 
   createEffect(() => {
-    console.log(`getAllEntityIdAndColor`, getAllEntityIdAndColor().map(i => i()))
-  })
+    console.log(`getAllEntityId`, entityChunkState.ids);
+  });
 
   createEffect(() => {
-    console.log(`getAllEntityIdAndCounter`, getAllEntityIdAndCounter().map(i => i()))
-  })
+    console.log(
+      `getAllEntityIdAndState`,
+      getAllEntityIdAndState().map((i) => i()),
+    );
+  });
 
-  createEffect(mapArray(getAllEntityIdAndCounter, (getEntityAndCounter) => createEffect(() => console.log(`getEntityIdAndCounter`, getEntityAndCounter()))))
-  createEffect(mapArray(getAllEntityIdAndColor, (getEntityAndColor) => createEffect(() => console.log(`getEntityIdAndColor`, getEntityAndColor()))))
+  createEffect(() => {
+    console.log(
+      `getAllEntityIdAndColor`,
+      getAllEntityIdAndColor().map((i) => i()),
+    );
+  });
+
+  createEffect(() => {
+    console.log(
+      `getAllEntityIdAndCounter`,
+      getAllEntityIdAndCounter().map((i) => i()),
+    );
+  });
+
+  createEffect(
+    mapArray(getAllEntityIdAndCounter, (getEntityAndCounter) =>
+      createEffect(() =>
+        console.log(`getEntityIdAndCounter`, getEntityAndCounter()),
+      ),
+    ),
+  );
+
+  createEffect(
+    mapArray(getAllEntityIdAndColor, (getEntityAndColor) =>
+      createEffect(() =>
+        console.log(`getEntityIdAndColor`, getEntityAndColor()),
+      ),
+    ),
+  );
 
   return { setState: setEntityChunkState };
 });
 
-console.group('add', 'a', 'start')
+console.group(`add`, `a`, `start`);
 setState(
-  reconcile(
-    {
-      ids: ["a"],
-      entities: {
-        ["a"]: {
-          id: "a",
-          counter: 1,
-          color: "red",
-        },
+  reconcile({
+    ids: [`a`],
+    entities: {
+      [`a`]: {
+        id: `a`,
+        counter: 1,
+        color: `red`,
       },
     },
-  ),
+  }),
 );
-console.groupEnd()
+console.groupEnd();
 
-console.group('add', 'b', 'start')
-setState(
-  (prev) =>
-    reconcile(
-      {
-        ids: [...prev.ids, "b"],
-        entities: {
-          ...prev.entities,
-          ["b"]: {
-            id: "b",
-            counter: 2,
-            color: "blue"
-          },
-        },
+console.group(`add`, `b`, `start`);
+setState((prev) =>
+  reconcile({
+    ids: [...prev.ids, `b`],
+    entities: {
+      ...prev.entities,
+      [`b`]: {
+        id: `b`,
+        counter: 2,
+        color: `blue`,
       },
-    )(prev),
+    },
+  })(prev),
 );
-console.groupEnd()
+console.groupEnd();
 
-console.group('set`, `counter', 'start')
-setState("entities", "b", "counter", 3)
-console.groupEnd()
+console.group(`set`, `counter`, `start`);
+setState(`entities`, `b`, `counter`, 3);
+console.groupEnd();
 
-console.group('set`, `color', 'start')
-setState("entities", "a", "color", "green")
-console.groupEnd()
+console.group(`set`, `color`, `start`);
+setState(`entities`, `a`, `color`, `green`);
+console.groupEnd();
