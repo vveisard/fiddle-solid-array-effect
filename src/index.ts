913d2cfcee3c46aa7b66cead5df2a588caf25024
Walter Blacke <walter.blacke@vegabyte.studio>
2024-02-07 18:44:51 +0000

    feature: borbs

--- src/index.ts
+++ src/index.ts
@@ -1,148 +1,239 @@
-import { createRoot, mapArray, createEffect, createMemo, onCleanup, onMount } from "solid-js";
-import { createStore, produce, reconcile } from "solid-js/store";
+import {
+  createMemo,
+  createRoot,
+  type Accessor,
+  createEffect,
+  createSignal,
+  mapArray,
+  indexArray,
+  onMount,
+  onCleanup,
+} from "solid-js";
+import { createStore, produce } from "solid-js/store";
 
-interface EntityState {
-  id: string;
-  counter: number;
-  color: string;
+// @region-base
+
+type ContentId = string;
+type EntityId = string;
+
+interface EntityCollection<T> {
+  readonly ids: Array<EntityId>;
+  readonly states: Record<EntityId, T>;
 }
 
-interface EntityChunkState {
-  ids: Array<string>;
-  entities: Record<string, EntityState>;
+// @region-end
+
+interface BorbEntityState {
+  readonly donkContentId: ContentId;
+  readonly color: string;
 }
 
-const { setState } = createRoot(() => {
-  const [entityChunkState, setEntityChunkState] = createStore<EntityChunkState>(
-    {
+interface DonkResourceData {}
+
+interface WorldResources {
+  readonly donkResources: Record<ContentId, DonkResourceData>;
+}
+
+interface WorldState {
+  readonly borbEntityCollection: EntityCollection<BorbEntityState>;
+}
+
+const root = createRoot(() => {
+  const [worldState, setWorldState] = createStore<WorldState>({
+    borbEntityCollection: {
       ids: [],
-      entities: {},
+      states: {},
     },
-  );
-
-  const getAllEntityIds = createMemo(
-    () => entityChunkState.ids
-  );
-
-  const getAllEntityIdAndState = createMemo(
-    mapArray(
-      getAllEntityIds,
-      (id, index) => () => [id, entityChunkState.entities[id]],
-    ),
-  );
-
-  const getAllEntityIdAndCounter = createMemo(
-    mapArray(
-      getAllEntityIds,
-      (id, index) => () => [id, entityChunkState.entities[id].counter],
-    ),
-  );
-
-  const getAllEntityIdAndColor = createMemo(
-    mapArray(
-      getAllEntityIds,
-      (id, index) => () => [id, entityChunkState.entities[id].color],
-    ),
-  );
-
-  createEffect(() => {
-    console.log(`getAllEntityId`, entityChunkState.ids);
   });
 
-  createEffect(() => {
-    console.log(
-      `getAllEntityIdAndState`,
-      getAllEntityIdAndState().map((i) => i()),
-    );
+  const [worldResources, setWorldResources] = createStore<WorldResources>({
+    donkResources: {},
   });
 
-  createEffect(() => {
-    console.log(
-      `getAllEntityIdAndColor`,
-      getAllEntityIdAndColor().map((i) => i()),
-    );
-  });
+  // @region-begin Basic
 
-  createEffect(() => {
-    console.log(
-      `getAllEntityIdAndCounter`,
-      getAllEntityIdAndCounter().map((i) => i()),
-    );
-  });
-
-  createEffect(
-    mapArray(getAllEntityIds, (getEntityId) =>
-      createEffect(() => {
-        console.log(`getEntityId`, getEntityId);
-        onMount(() => console.log(`getEntityId`, getEntityId, `onMount`));
-        onCleanup(() => console.log(`getEntityId`, getEntityId, `onCleanup`));
-      }),
-    ),
+  const getAllBorbEntityIds: Accessor<Array<EntityId>> = createMemo(
+    () => worldState.borbEntityCollection.ids
   );
 
   createEffect(
-    mapArray(getAllEntityIdAndCounter, (getEntityAndCounter) =>
-      createEffect(() => {
-        console.log(`getEntityIdAndCounter`, getEntityAndCounter());
-      }),
-    ),
+    mapArray(getAllBorbEntityIds, (borbEntityId, getIndex) => {
+      onMount(() => {
+        console.log(
+          `getAllBorbEntityIds`,
+          `mapArray/onMount`,
+          `track value ${borbEntityId} at index ${getIndex()}`
+        );
+      });
+
+      onCleanup(() => {
+        console.log(
+          `getAllBorbEntityIds`,
+          `mapArray/onCleanup`,
+          `untrack value ${borbEntityId} at index ${getIndex()}`
+        );
+      });
+
+      createEffect((prevIndexOfValue: number | undefined) => {
+        const currentIndexOfValue = getIndex();
+        console.log(
+          `getAllBorbEntityIds`,
+          `mapArray/createEffect`,
+          `value ${borbEntityId} changed index from ${prevIndexOfValue} to ${currentIndexOfValue}`
+        );
+
+        return currentIndexOfValue;
+      });
+    })
   );
 
   createEffect(
-    mapArray(getAllEntityIdAndColor, (getEntityAndColor) =>
-      createEffect(() => {
-        console.log(`getEntityIdAndColor`, getEntityAndColor());
-      }
-      ),
-    ),
+    indexArray(getAllBorbEntityIds, (getBorbEntityId, index) => {
+      onMount(() => {
+        console.log(
+          `getAllBorbEntityIds`,
+          `indexArray/onMount`,
+          `track index ${index} with value ${getBorbEntityId()}`
+        );
+      });
+
+      onCleanup(() => {
+        console.log(
+          `getAllBorbEntityIds`,
+          `indexArray/onCleanup`,
+          `untrack index ${index} with value ${getBorbEntityId()}`
+        );
+      });
+
+      createEffect((prevValueAtIndex: string | undefined) => {
+        const currentValueAtIndex = getBorbEntityId();
+        console.log(
+          `getAllBorbEntityIds`,
+          `indexArray/createEffect`,
+          `index ${index} changed value from ${prevValueAtIndex} to ${currentValueAtIndex}`
+        );
+
+        return currentValueAtIndex;
+      });
+    })
   );
 
-  return { setState: setEntityChunkState };
+  // @region-end
+
+  // @region-begin
+
+  const getAllBorbEntityIdAndGetColor: Accessor<
+    Array<[EntityId, Accessor<string | undefined>]>
+  > = mapArray(getAllBorbEntityIds, (borbContentId) => [
+    borbContentId,
+    () => worldState.borbEntityCollection.states[borbContentId]?.color,
+  ]) as Accessor<Array<[EntityId, Accessor<string>]>>;
+
+  createEffect(
+    mapArray(getAllBorbEntityIdAndGetColor, ([borbEntityId, getColor]) => {
+      onMount(() => {
+        console.log(
+          `getAllBorbEntityIdAndGetColor`,
+          `mapArray/onMount`,
+          `track value ${borbEntityId} with color ${getColor()}`
+        );
+      });
+
+      onCleanup(() => {
+        console.log(
+          `getAllBorbEntityIdAndGetColor`,
+          `mapArray/onCleanup`,
+          `untrack value ${borbEntityId} with color ${getColor()}`
+        );
+      });
+
+      createEffect((prevColor: string | undefined) => {
+        const currentColor = getColor();
+        console.log(
+          `getAllBorbEntityIdAndGetColor`,
+          `mapArray/createEffect`,
+          `value ${borbEntityId} changed color from ${prevColor} to ${currentColor}`
+        );
+
+        return currentColor;
+      });
+    })
+  );
+
+  // @region-end
+
+  return {
+    setWorldState,
+    setWorldResources,
+  };
 });
 
-console.group(`add`, `a`, `start`);
-setState(
-  reconcile({
-    ids: [`a`],
-    entities: {
-      [`a`]: {
-        id: `a`,
-        counter: 1,
-        color: `red`,
-      },
-    },
-  }),
+console.group(`create "a:cool"`);
+root.setWorldState(
+  produce((state) => {
+    state.borbEntityCollection.ids.push("a:cool");
+    state.borbEntityCollection.states["a:cool"] = {
+      color: "red",
+      donkContentId: "sunglasses",
+    };
+  })
 );
 console.groupEnd();
 
-console.group(`add`, `b`, `start`);
-setState((prev) =>
-  reconcile({
-    ids: [...prev.ids, `b`],
-    entities: {
-      ...prev.entities,
-      [`b`]: {
-        id: `b`,
-        counter: 2,
-        color: `blue`,
-      },
-    },
-  })(prev),
+console.group(`create "b:rude"`);
+root.setWorldState(
+  produce((state) => {
+    state.borbEntityCollection.ids.push("b:rude");
+    state.borbEntityCollection.states["b:rude"] = {
+      color: "yellow",
+      donkContentId: "cigarette",
+    };
+  })
 );
 console.groupEnd();
 
-console.group(`set`, `counter`, `start`);
-setState(`entities`, `b`, `counter`, 3);
+console.group(`create "c:fun"`);
+root.setWorldState(
+  produce((state) => {
+    state.borbEntityCollection.ids.push("c:fun");
+    state.borbEntityCollection.states["c:fun"] = {
+      color: "purple",
+      donkContentId: "party-hat",
+    };
+  })
+);
 console.groupEnd();
 
-console.group(`set`, `color`, `start`);
-setState(`entities`, `a`, `color`, `green`);
+console.group(`delete "b:rude"`);
+root.setWorldState(
+  produce((state) => {
+    state.borbEntityCollection.ids.splice(
+      state.borbEntityCollection.ids.indexOf("b:rude"),
+      1
+    );
+    delete state.borbEntityCollection.states["b:rude"];
+  })
+);
 console.groupEnd();
 
+console.group("create d:smart");
+root.setWorldState(
+  produce((state) => {
+    state.borbEntityCollection.ids.push("d:smart");
+    state.borbEntityCollection.states["d:smart"] = {
+      color: "green",
+      donkContentId: "glasses",
+    };
+  })
+);
+console.groupEnd();
 
-console.group(`remove`, `a`, `start`);
-setState(produce(prev => {
-  prev.ids.splice(prev.ids.indexOf('a'), 1)
-  delete prev.entities['a']
-}));
+console.group("change a:cool color");
+root.setWorldState(
+  produce((state) => {
+    state.borbEntityCollection.states["a:cool"] = {
+      color: "orange",
+    };
+  })
+);
 console.groupEnd();
